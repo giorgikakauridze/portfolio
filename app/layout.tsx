5c5fd149ecef18a44194697ed20d73ef4409e238
giorgikakauridze <giorgi.kakauridze.00@gmail.com>
2025-11-14 15:12:51 +0400

    focm

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className=" bg-[#141128]">
       <link rel="icon" href="/icon.svg" />
       <meta
         name="google-site-verification"
